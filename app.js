5e837c0dfaf69bbef2784133a16fa1099968ab0d
bilalashrafali123 <ashrafalibilal385@gmail.com>
2024-05-23 20:42:30 +0500

    done

--- app.js
+++ app.js
@@ -6,27 +6,54 @@ const age = document.querySelector('#age')
 const nic = document.querySelector('#cnic')
 
 
-
-const usernameRegex = /^[a-zA-Z0-9]{4,15}$/;
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})/;
+const userNameRegex = /^[a-zA-Z0-9_]{4,15}$/;
+const emailRegex = /^(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*._%+-]{8,}@[A-Za-z\d.-]+\.[A-Za-z]{2,}$/;
+const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
 const ageRegex = /^(1[89]|[2-5][0-9]|60)$/;
 const cnicRegex = /^42\d{11}$/;
 
 
+
+
 form.addEventListener('submit' , function(event){
+
     event.preventDefault()
-    // console.log(userName.value);
-    // console.log(email.value);
-    // console.log(password.value);
-    // console.log(age.value);
-    // console.log(nic.value);
 
-    console.log(usernameRegex.test(userName.value));
-    console.log(emailRegex.test(email.value));
-    console.log(passwordRegex.test(password.value));
-    console.log(ageRegex.test(age.value));
-    console.log(cnicRegex.test(nic.value));
+    if(userNameRegex.test(userName.value)){
 
-       
-  }  )
+        console.log(userName.value);
+    }else{
+        console.log('please enter valid username');
+    }
+
+    if(emailRegex.test(email.value)){
+        console.log(email.value);
+    }else{
+        console.log('please enter valid email');
+    }
+
+
+    if(passwordRegex.test(password.value)){
+
+        console.log(password.value);
+    }else{
+        console.log('please enter valid password');
+    }
+
+    if(ageRegex.test(age.value)){
+
+        console.log(age.value);
+    }else{
+        console.log('please enter valid age');
+    }
+
+    if(cnicRegex.test(nic.value)){
+
+        console.log(nic.value);
+    }else{
+        console.log('please enter valid nic number');
+    }
+
+
+})
+
